fa6daff1988024c1154163137bf6c9ebb6594d43
aZong <giongmua2004@gmail.com>
2025-03-26 21:40:19 +0700

    refactor: 💡 remove vue devtool

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,9 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vueDevTools from 'vite-plugin-vue-devtools';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue()],
   server: {
     port: 3000,
   },
