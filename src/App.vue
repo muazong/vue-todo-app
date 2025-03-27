c441793ab2e581f182df05957e7ea913c40da482
aZong <giongmua2004@gmail.com>
2025-03-27 08:23:07 +0700

    feat: 🎸 use vue router

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div></div>
+  <div class="todo-app">
+    <div class="header">
+      <h1>Vue - Todo App</h1>
+    </div>
+
+    <RouterView />
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.header {
+  text-align: center;
+}
+</style>
