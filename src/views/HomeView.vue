daed289d66cc7e6dc2e9a7d056b311fbf79ac4db
aZong <giongmua2004@gmail.com>
2025-03-27 08:24:02 +0700

    feat: 🎸 add view page

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import TodoList from '@/component/Todo/TodoList.vue';
+import TodoInput from '@/component/TodoInput.vue';
+</script>
+
+<template>
+  <div>
+    <TodoInput />
+    <hr />
+    <TodoList />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/MoreView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <div>
+    <RouterLink to="/">Home</RouterLink>
+    <h1>More Page</h1>
+  </div>
+</template>
+
+<style scoped></style>
